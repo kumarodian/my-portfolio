636229be090fcaf304410cdf411e2c17a7a6c11e
Sachin <sachinyadav607@gmail.com>
2021-12-25 11:50:35 +0530

    First running version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Header from './Component/Header/Header';
-import Home from './Component/Home/Home';
-import Intro from './Component/Intro/Intro';
-import Expertise from './Component/Expertise';
+import "./App.css";
+import Header from "./Component/Header/Header";
+import Home from "./Component/Home/Home";
+import Intro from "./Component/Intro/Intro";
+import Expertise from "./Component/Expertise/Expertise";
 /*import Qualification from './Component/Qualtification';
 import RecentWork from './Component/RecentWork';
 import ContactMe from './Component/ContactMe';
@@ -12,16 +12,15 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <Header/>
-      <Home/>
-      <Intro/>
-      <Expertise/>
-      {/* <Qualification></Qualification>
+        <Header />
+        <Home />
+        <Intro />
+        <Expertise />
+        {/* <Qualification></Qualification>
       <RecentWork></RecentWork>
       <ContactMe></ContactMe>
       <Footer></Footer> */}
       </div>
-
     </div>
   );
 }
--- src/Component/Expertise/Expertise.js
+++ src/Component/Expertise/Expertise.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-import './Expertise.css';
+import React, { Component } from "react";
+import "./Expertise.css";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-            <p className="color">Why Choose Me</p>
-            <p className="aboutMeTag">About Me</p>
-            <div className="row">
-                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-
-                </div>
-                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-
-                </div>
-            </div>
-            </div>
-        )
-    }
+class Expertise extends Component {
+  render() {
+    return (
+      <div>
+        <p className="color">Why Choose Me</p>
+        <p className="aboutMeTag">About Me</p>
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12"></div>
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12"></div>
+        </div>
+      </div>
+    );
+  }
 }
-export default App;
+export default Expertise;
